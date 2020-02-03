ab6805473b7104ef2ee387169f9261c2270e8682
HUSTAR_04 <aosxn2@naver.com>
2020-02-03 16:42:15 +0900

    init commit

---
+++ src/main/java/hustime/HustimeWebApplication.java
@@ -0,0 +1,13 @@
+package hustime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HustimeWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HustimeWebApplication.class, args);
+	}
+
+}
