aa8545ee34d04aa8c997b89248b805608b711453
baejny <cjlmdy31@naver,com>
2020-02-13 12:25:08 +0900

    02131225 baejny

--- src/main/java/hustime/controller/IndexController.java
+++ src/main/java/hustime/controller/IndexController.java
@@ -1,5 +1,7 @@
 package hustime.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
@@ -7,12 +9,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import hustime.member.configuration.auth.dto.SessionUser;
+import hustime.member.domain.User;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 @Controller
 public class IndexController {
-	
+	private final HttpSession httpSession;
 	@RequestMapping(value="/", method=RequestMethod.GET)
 	public ModelAndView openBoardList(ModelMap model) throws Exception{
 		ModelAndView mv = new ModelAndView("/index");
+		User user = (User)httpSession.getAttribute("user");
 		return mv;
 	}
 	
