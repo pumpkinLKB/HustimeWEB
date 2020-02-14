0171b7a75ca53a105b0695308a1973aa2433d471
HUSTAR_04 <aosxn2@naver.com>
2020-02-14 17:29:02 +0900

    pumpkinLKB

--- src/main/java/hustime/controller/IndexController.java
+++ src/main/java/hustime/controller/IndexController.java
@@ -2,6 +2,9 @@ package hustime.controller;
 
 import javax.servlet.http.HttpSession;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
@@ -15,9 +18,15 @@ import hustime.member.configuration.auth.dto.SessionUser;
 import hustime.member.domain.user.User;
 import lombok.RequiredArgsConstructor;
 
+import hustime.community.schedule.service.ScheduleService;
+import hustime.community.schedule.dto.*;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
+	
+	@Autowired
+	private ScheduleService scheduleService;
 	
 	@GetMapping("/1")
 	public String index(Model model, @LoginUser SessionUser user) {
@@ -25,12 +34,15 @@ public class IndexController {
 	}
 	
 	@RequestMapping(value = "/", method=RequestMethod.GET) 
-	public String templateloginPage(Model model, @LoginUser SessionUser user) {
+	public ModelAndView templateloginPage(Model model, @LoginUser SessionUser user) throws Exception{
 		System.out.println("USER: "+user);
 		if(user!=null) {
 			model.addAttribute("userName", user.getName());
 		}
-		return "index1";
+		ModelAndView mv = new ModelAndView("/index1");
+		List<ScheduleDto> list = scheduleService.selectBoardList();
+		mv.addObject("list", list);
+		return mv;
 	}
 	
 }
