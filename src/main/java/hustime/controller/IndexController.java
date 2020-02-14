d66d7c82f1c5bd39f519e67f3164f746f0ffb604
HUSTAR_04 <aosxn2@naver.com>
2020-02-14 17:09:27 +0900

    pumpkinLKB

--- src/main/java/hustime/controller/IndexController.java
+++ src/main/java/hustime/controller/IndexController.java
@@ -1,5 +1,8 @@
 package hustime.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
@@ -7,12 +10,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import hustime.community.schedule.service.ScheduleService;
+import hustime.community.schedule.dto.*;
 @Controller
 public class IndexController {
+	
+	@Autowired
+	private ScheduleService scheduleService;
 	
 	@RequestMapping(value="/", method=RequestMethod.GET)
 	public ModelAndView openBoardList(ModelMap model) throws Exception{
 		ModelAndView mv = new ModelAndView("/index");
+		List<ScheduleDto> list = scheduleService.selectBoardList();
+		mv.addObject("list", list);
 		return mv;
 	}
 	
