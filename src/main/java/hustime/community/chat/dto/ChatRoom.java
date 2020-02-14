746d1c9726e6335242354b9518d398889257d3fa
HUSTAR_04 <aosxn2@naver.com>
2020-02-14 11:40:52 +0900

    pumpkinLKB chat+restaurant

---
+++ src/main/java/hustime/community/chat/dto/ChatRoom.java
@@ -0,0 +1,18 @@
+package hustime.community.chat.dto;
+
+import java.util.UUID;
+
+import lombok.Data;
+
+@Data
+public class ChatRoom {
+    private String roomId;
+    private String name;
+ 
+    public static ChatRoom create(String name) {
+        ChatRoom chatRoom = new ChatRoom();
+        chatRoom.roomId = UUID.randomUUID().toString();
+        chatRoom.name = name;
+        return chatRoom;
+    }
+}
